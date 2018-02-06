403d1517cd9a3f147502e707414034bf776f1b3d
Mason <masonjterry@gmail.com>
2018-02-05 18:44:42 -0600

    deleted additem file

--- client/src/pages/Errands/Errands.js
+++ client/src/pages/Errands/Errands.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AddItem from "./AddItem";
+import { Modal } from "react-modal";
 
 const style = {
   margin: 20,
@@ -9,13 +9,30 @@ const style = {
 }
 
 export default class AddErrands extends React.Component {
+
+  state = {
+    modalIsOpen: false
+  };
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true })
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false })
+  };
+
   render() {
     return (
       <div style={style}>
         <h1>Add Your Errands</h1>
         <hr />
-        <button style={style.buttonStyle} className="mdc-fab material-icons"><span className="mdc-fab__icon">add</span></button>
-        <AddItem open="false"/>
+        <button style={style.buttonStyle} onClick={this.openModal} className="mdc-fab material-icons"><span className="mdc-fab__icon">add</span></button>
+        <Modal isOpen={this.state.modalIsOpen}>
+          <button onClick={this.closeModal}>X</button>
+          <h1>Test</h1>
+          <p>Test</p>
+        </Modal>
       </div>
     );
   }
