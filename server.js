4021b1d7a79e40e0ba01cf67821b12d4e648427d
Mason <masonjterry@gmail.com>
2018-02-19 23:12:33 -0600

    starting to set up database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
