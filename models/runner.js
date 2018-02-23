56f460e39ee65a997b557cb241ac977624aa583c
Mason <masonjterry@gmail.com>
2018-02-22 22:48:10 -0600

    added runner schema file

--- models/runner.js
+++ models/runner.js
@@ -2,7 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const runner = new Schema({
-
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  streetName: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  birthday: { type: String, required: true }
 });
 
 const Runner = mongoose.mogel("Runner", runner);
+
+module.exports = Runner;
