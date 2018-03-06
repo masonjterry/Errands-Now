196165c5d55409235da646a0333b102e5203e396
Mason <masonjterry@gmail.com>
2018-03-05 19:49:45 -0600

    working on routes, controllers

--- server.js
+++ server.js
@@ -24,10 +24,6 @@ mongoose.connect(
 
 app.use(routes);
 
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
 app.listen(PORT, function() {
   console.log(`Listening on port ${PORT}!`);
 });
